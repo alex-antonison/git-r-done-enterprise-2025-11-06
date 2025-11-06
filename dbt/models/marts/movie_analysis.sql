3d52d2fa658f34cc9fd453f97d3728a2ba5cb720
OliNEWMAN <olivier.newman@theinformationlab.co.uk>
2025-11-06 16:39:48 +0000

    'highest grossing film in 2024'

---
+++ dbt/models/marts/movie_analysis.sql
@@ -0,0 +1,22 @@
+with
+
+temp_cte AS (
+    SELECT
+        release_year,
+        mpaa_rating,
+        major_genres,
+        worldwide_gross
+    FROM {{ ref('stg_movies') }}
+)
+
+
+SELECT
+    release_year,
+    major_genres,
+    sum(worldwide_gross) AS total_worldwide_gross
+FROM temp_cte
+where release_year = 2024
+GROUP BY
+    release_year,
+    major_genres
+   
